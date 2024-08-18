e1e0ab73de1fb2898ffbe709c45024aa688f9d39
Nikhil Rana <nikhilrajput060@gmail.com>
2024-08-18 12:23:59 +0530

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,33 @@
+const videos = [
+    {
+        title: 'Sarpanchi 2024 - Official Video | Jass Bajwa | New Punjabi Song 2024',
+        duration: '00:02:37',
+        file: 'Y2meta.app-Sarpanchi 2024 - Official Video _ Jass Bajwa _ New Punjabi Song 2024.mp4',
+        Img: 'sarpanchi.jpg',
+    },
+    {
+        title: 'Dhanda Nyoliwala - RUBICON (Official Music Video)',
+        duration: '00:02:37',
+        file: 'Y2meta.app-Dhanda Nyoliwala - RUBICON (Official Music Video).mp4',
+        Img: 'dhanda.jpg',
+    },
+
+    {
+        title: 'Ambarsaria ( Video ) Navaan Sandhu | HomeBoy | Kaater | Lavish Dhiman | New Punjabi songs 2024',
+        duration: '00:02:37',
+        file: 'Y2meta.app-Ambarsaria ( Video )  Navaan Sandhu _ HomeBoy _ Kaater _ Lavish Dhiman _ New Punjabi songs 2024.mp4',
+        Img: 'navaan.jpg',
+    },
+    {
+        title: 'Sarpanchi 2024 - Official Video | Jass Bajwa | New Punjabi Song 2024',
+        duration: '00:02:37',
+        file: 'Y2meta.app-Sarpanchi 2024 - Official Video _ Jass Bajwa _ New Punjabi Song 2024.mp4',
+        Img: 'sarpanchi.jpg',
+    },
+    {
+        title: 'Sarpanchi 2024 - Official Video | Jass Bajwa | New Punjabi Song 2024',
+        duration: '00:02:37',
+        file: 'Y2meta.app-Sarpanchi 2024 - Official Video _ Jass Bajwa _ New Punjabi Song 2024.mp4',
+        Img: 'sarpanchi.jpg',
+    }
+];
